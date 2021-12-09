402e52139677c7872e3b10f0e7dcdb113a37df2b
Ellen <eol50559@gmail.com>
2021-12-09 22:27:32 +0000

    refactored to use MainApp.kt

--- app/src/main/java/com/example/petminder/activities/PetActivity.kt
+++ app/src/main/java/com/example/petminder/activities/PetActivity.kt
@@ -3,8 +3,8 @@ package com.example.petminder.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.petminder.databinding.ActivityPetBinding
+import com.example.petminder.main.MainApp
 import com.example.petminder.models.PetModel
-import com.github.ajalt.timberkt.Timber
 import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber.i
 
@@ -12,16 +12,19 @@ class PetActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPetBinding
     var pet = PetModel()
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPetBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Timber.plant(Timber.DebugTree())
+        app = application as MainApp
         i("Pet Minder Activity started....")
         binding.btnAdd.setOnClickListener(){
             pet.name = binding.petName.text.toString()
             if (pet.name.isNotEmpty()) {
+                app.pets.add(pet.copy())
                 i("add Button Pressed")
             } else{
                 Snackbar.make(it,"Please enter a name", Snackbar.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/petminder/main/MainApp.kt
@@ -0,0 +1,18 @@
+package com.example.petminder.main
+
+import android.app.Application
+import com.example.petminder.models.PetModel
+import com.github.ajalt.timberkt.Timber
+import timber.log.Timber.i
+
+class MainApp: Application() {
+
+    val pets = ArrayList<PetModel>()
+    override fun onCreate(){
+        super.onCreate()
+        Timber.plant(
+            Timber.DebugTree()
+        )
+        i("Placemark started")
+    }
+}
