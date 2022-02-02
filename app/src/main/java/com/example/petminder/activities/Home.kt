eb7d7b677bfe1a0a5c6946a7acee35c8f8f4e11e
Ellen <eol50559@gmail.com>
2022-02-02 18:44:20 +0000

    basic nav drawer

---
+++ app/src/main/java/com/example/petminder/activities/Home.kt
@@ -0,0 +1,43 @@
+package com.example.petminder.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.example.petminder.R
+import com.example.petminder.databinding.HomeBinding
+
+class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding: HomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.petAddFragment, R.id.petListFragment
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/petminder/fragments/PetListFragment.kt
@@ -0,0 +1,68 @@
+package com.example.petminder.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.petminder.R
+import com.example.petminder.adapters.PetAdapter
+import com.example.petminder.adapters.PetListener
+import com.example.petminder.databinding.FragmentPetListBinding
+import com.example.petminder.main.MainApp
+import com.example.petminder.models.PetModel
+import timber.log.Timber
+
+class PetListFragment : PetListener, Fragment() {
+
+    lateinit var app : MainApp
+    private var _fragBinding: FragmentPetListBinding? = null
+    private val fragBinding get() = _fragBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = activity?.application as MainApp
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragBinding = FragmentPetListBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        fragBinding.recyclerView.setLayoutManager(LinearLayoutManager(activity))
+        fragBinding.recyclerView.adapter = PetAdapter(app.pets.findAll(), this)
+
+        return root
+    }
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            PetListFragment().apply {
+                arguments = Bundle().apply { }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragBinding = null
+    }
+
+    override fun onPetClick(pet: PetModel) {
+        Timber.i("Clicked on pet")
+        TODO("Add Pet click")
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_add, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+}
