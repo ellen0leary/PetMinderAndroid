f3d8ac72a8c47f32ad7033adf3b6e4fdae23ee79
Ellen <eol50559@gmail.com>
2022-03-29 10:45:13 +0100

    user can register, login and sign out

--- app/src/main/java/com/example/petminder/ui/auth/LoggedInViewModel.kt
+++ app/src/main/java/com/example/petminder/ui/auth/LoggedInViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.petminder.ui.auth
 
-class LoggedInViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.petminder.firebase.FirebaseAuthManager
+import com.google.firebase.auth.FirebaseUser
+
+class LoggedInViewModel(app: Application): AndroidViewModel(app){
+    var firebaseAuthManager: FirebaseAuthManager = FirebaseAuthManager(app)
+    var liveFirebaseUser: MutableLiveData<FirebaseUser> = firebaseAuthManager.liveFirebaseUser
+    var loggedOut : MutableLiveData<Boolean> = firebaseAuthManager.loggedOut
+
+    fun logOut(){
+        firebaseAuthManager.logOut()
+    }
 }
---
+++ app/src/main/java/com/example/petminder/ui/home/Home.kt
@@ -0,0 +1,93 @@
+package com.example.petminder.ui.home
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.example.petminder.R
+import com.example.petminder.databinding.HomeBinding
+import com.example.petminder.databinding.NavHeaderBinding
+import com.example.petminder.ui.auth.LoggedInViewModel
+import com.example.petminder.ui.auth.Login
+import com.google.firebase.auth.FirebaseUser
+import timber.log.Timber
+
+class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding: HomeBinding
+    private lateinit var navHeaderBinding : NavHeaderBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var loggedInViewModel : LoggedInViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        toolbar.title = "PetMinder"
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.petAddFragment, R.id.petListFragment
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
+            if (firebaseUser != null) {
+                Timber.i("null firebase user")
+                //val currentUser = loggedInViewModel.liveFirebaseUser.value
+                /*if (currentUser != null)*/ updateNavHeader(loggedInViewModel.liveFirebaseUser.value!!)
+            } else {
+                Timber.i("null firebase user")
+            }
+        })
+
+        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+            Timber.i(loggedout.toString())
+            if (loggedout) {
+                Timber.i("here")
+                startActivity(Intent(this, Login::class.java))
+            } else {
+                Timber.i("logged in")
+            }
+        })
+
+    }
+
+    private fun updateNavHeader(currentUser: FirebaseUser) {
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderBinding = NavHeaderBinding.bind(headerView)
+//        navHeaderBinding.navHeaderEmail.text = currentUser.email
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    fun signOut(item: MenuItem) {
+        loggedInViewModel.logOut()
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+}
